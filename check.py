94196f22f904876b7544c50875bacb07d3e16c24
wsx7797 <taha9727@gmail.com>
2026-05-06 23:55:52 +0300

    Create check.py

---
+++ check.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import urllib.parse
+import time
+
+URL = "https://www.tcf.gov.tr/branslar/pilates/"
+
+def send_whatsapp(msg):
+    phone = os.getenv("PHONE")
+    apikey = os.getenv("APIKEY")
+
+    text = urllib.parse.quote(msg)
+
+    url = f"https://api.callmebot.com/whatsapp.php?phone={phone}&text={text}&apikey={apikey}"
+    requests.get(url)
+
+print("Bot başladı...")
+
+while True:
+    try:
+        r = requests.get(URL)
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        rows = soup.find_all("tr")
+
+        for row in rows:
+            text = row.get_text()
+
+            if "Ankara" in text and "İncele" in text:
+                send_whatsapp(f"🔥 Ankara kursu açıldı!\n{text}")
+
+    except Exception as e:
+        print("Hata:", e)
+
+    time.sleep(30)
